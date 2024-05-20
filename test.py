97eb6f18106f728c47905620a31b7bf23c571b46
Sakan <sakanbeer@gmail.com>
2024-05-21 03:04:40 +0700

    v2.0.0 - Added Scraper class to scrape.py - Adjusted test.py - Refactored automated_scraper.py, scrape_until_month_end.py, and thread_scrape.py - Added set_details.py - Added parsers to control which scraper to use

--- test.py
+++ test.py
@@ -24,105 +24,105 @@ from japan_avg_hotel_price_finder.scrape_until_month_end import MonthEndBasicScr
 from japan_avg_hotel_price_finder.thread_scrape import ThreadPoolScraper
 
 
-# def test_thread_scraper() -> None:
-#     city = 'Osaka'
-#     group_adults = 1
-#     num_rooms = 1
-#     group_children = 0
-#     selected_currency = 'USD'
-#
-#     today = datetime.date.today()
-#     start_day = 27
-#     month = today.month
-#     year = today.year
-#     last_day: int = calendar.monthrange(year, month)[1]
-#     nights = 1
-#
-#     sqlite_name = 'test.db'
-#
-#     hotel_stay = Details(
-#         city=city, group_adults=group_adults, num_rooms=num_rooms,
-#         group_children=group_children, selected_currency=selected_currency,
-#         start_day=start_day, month=month, year=year, nights=nights, sqlite_name=sqlite_name
-#     )
-#
-#     thread_scrape = ThreadPoolScraper(hotel_stay)
-#     df = thread_scrape.thread_scrape()
-#
-#     target_date_range: list[str] = [datetime.date(year, month, day).strftime('%Y-%m-%d') for day in
-#                                     range(start_day, last_day + 1)]
-#     unique_dates: list[str] = list(df['Date'].unique())
-#
-#     target_date_range.sort()
-#     unique_dates.sort()
-#
-#     assert not df.empty
-#     assert target_date_range == unique_dates
-#
-#
-# def test_until_month_end_scraper() -> None:
-#     city = 'Osaka'
-#     group_adults = 1
-#     num_rooms = 0
-#     group_children = 0
-#     selected_currency = 'USD'
-#
-#     today = datetime.date.today()
-#     start_day = 27
-#     month = today.month
-#     year = today.year
-#     last_day: int = calendar.monthrange(year, month)[1]
-#     nights = 1
-#
-#     sqlite_name = 'test.db'
-#
-#     hotel_stay = Details(
-#         city=city, group_adults=group_adults, num_rooms=num_rooms,
-#         group_children=group_children, selected_currency=selected_currency,
-#         start_day=start_day, month=month, year=year, nights=nights, sqlite_name=sqlite_name
-#     )
-#
-#     month_end = MonthEndBasicScraper(hotel_stay)
-#     df = month_end.scrape_until_month_end()
-#
-#     target_date_range: list[str] = [datetime.date(year, month, day).strftime('%Y-%m-%d')
-#                                     for day in range(start_day, last_day + 1)]
-#
-#     unique_dates: list[str] = list(df['Date'].unique())
-#
-#     target_date_range.sort()
-#     unique_dates.sort()
-#
-#     assert not df.empty
-#     assert target_date_range == unique_dates
-#
-#
-# def test_scraper() -> None:
-#     city = 'Osaka'
-#     group_adults = 1
-#     num_rooms = 1
-#     group_children = 0
-#     selected_currency = 'USD'
-#
-#     today = datetime.date.today()
-#     month = today.month
-#     year = today.year
-#     check_in = datetime.date(year, month, today.day).strftime('%Y-%m-%d')
-#     check_out = datetime.date(year, month, today.day) + datetime.timedelta(days=1)
-#     check_out = check_out.strftime('%Y-%m-%d')
-#
-#     sqlite_name = 'test.db'
-#
-#     hotel_stay = Details(
-#         city=city, group_adults=group_adults, num_rooms=num_rooms,
-#         group_children=group_children, selected_currency=selected_currency,
-#         month=month, year=year, sqlite_name=sqlite_name
-#     )
-#
-#     scraper = BasicScraper(hotel_stay)
-#     df = scraper.start_scraping_process(check_in, check_out)
-#
-#     assert not df.empty
+def test_thread_scraper() -> None:
+    city = 'Osaka'
+    group_adults = 1
+    num_rooms = 1
+    group_children = 0
+    selected_currency = 'USD'
+
+    today = datetime.date.today()
+    start_day = 27
+    month = today.month
+    year = today.year
+    last_day: int = calendar.monthrange(year, month)[1]
+    nights = 1
+
+    sqlite_name = 'test.db'
+
+    hotel_stay = Details(
+        city=city, group_adults=group_adults, num_rooms=num_rooms,
+        group_children=group_children, selected_currency=selected_currency,
+        start_day=start_day, month=month, year=year, nights=nights, sqlite_name=sqlite_name
+    )
+
+    thread_scrape = ThreadPoolScraper(hotel_stay)
+    df = thread_scrape.thread_scrape()
+
+    target_date_range: list[str] = [datetime.date(year, month, day).strftime('%Y-%m-%d') for day in
+                                    range(start_day, last_day + 1)]
+    unique_dates: list[str] = list(df['Date'].unique())
+
+    target_date_range.sort()
+    unique_dates.sort()
+
+    assert not df.empty
+    assert target_date_range == unique_dates
+
+
+def test_until_month_end_scraper() -> None:
+    city = 'Osaka'
+    group_adults = 1
+    num_rooms = 0
+    group_children = 0
+    selected_currency = 'USD'
+
+    today = datetime.date.today()
+    start_day = 27
+    month = today.month
+    year = today.year
+    last_day: int = calendar.monthrange(year, month)[1]
+    nights = 1
+
+    sqlite_name = 'test.db'
+
+    hotel_stay = Details(
+        city=city, group_adults=group_adults, num_rooms=num_rooms,
+        group_children=group_children, selected_currency=selected_currency,
+        start_day=start_day, month=month, year=year, nights=nights, sqlite_name=sqlite_name
+    )
+
+    month_end = MonthEndBasicScraper(hotel_stay)
+    df = month_end.scrape_until_month_end()
+
+    target_date_range: list[str] = [datetime.date(year, month, day).strftime('%Y-%m-%d')
+                                    for day in range(start_day, last_day + 1)]
+
+    unique_dates: list[str] = list(df['Date'].unique())
+
+    target_date_range.sort()
+    unique_dates.sort()
+
+    assert not df.empty
+    assert target_date_range == unique_dates
+
+
+def test_scraper() -> None:
+    city = 'Osaka'
+    group_adults = 1
+    num_rooms = 1
+    group_children = 0
+    selected_currency = 'USD'
+
+    today = datetime.date.today()
+    month = today.month
+    year = today.year
+    check_in = datetime.date(year, month, today.day).strftime('%Y-%m-%d')
+    check_out = datetime.date(year, month, today.day) + datetime.timedelta(days=1)
+    check_out = check_out.strftime('%Y-%m-%d')
+
+    sqlite_name = 'test.db'
+
+    hotel_stay = Details(
+        city=city, group_adults=group_adults, num_rooms=num_rooms,
+        group_children=group_children, selected_currency=selected_currency,
+        month=month, year=year, sqlite_name=sqlite_name
+    )
+
+    scraper = BasicScraper(hotel_stay)
+    df = scraper.start_scraping_process(check_in, check_out)
+
+    assert not df.empty
 
 
 def test_weekly_scraper() -> None:
