773cf42924668f458693b6b3234e7a43cfe911d0
Sakan <sakanbeer@gmail.com>
2024-06-02 01:27:04 +0700

    v2.5.0 - Added to_sqlite flag argument - Added logic to prevent scraping the day that already passed for scrape_until_month_end.py and thread_scrape.py - Added utils.py

--- japan_avg_hotel_price_finder/utils.py
+++ japan_avg_hotel_price_finder/utils.py
@@ -14,7 +14,7 @@ def check_if_current_date_has_passed(year, month, day):
     today_for_check = datetime.today().strftime('%Y-%m-%d')
     current_date_for_check = datetime(year, month, day).strftime('%Y-%m-%d')
     if current_date_for_check < today_for_check:
-        logger.warning(f'The current day of the month to scrape was passed. Skip this day.')
+        logger.warning(f'The current day of the month to scrape was passed. Skip {year}-{month}-{day}.')
         return True
     else:
         return False
