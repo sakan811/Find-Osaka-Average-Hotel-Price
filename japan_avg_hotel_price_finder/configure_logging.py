919ff5bad90aba710bea83404052fcd28a74f417
Sakan <sakanbeer@gmail.com>
2024-06-15 00:34:48 +0700

    v3.9.0 - Added --workers to adjust pool thread workers for Thread Pool Scraper - Adjusted default pool thread workers to 5 - Use Firefox instead of Chrome - Adjusted wait time for clicking pop-up ad and load more result button while scrolling down to 0.5 second - Adjusted scroll-down length to 2,000 pixels - Use logging instead of loguru - Adjusted scripts to use the same logger for all scrapers - Added driver wait for clicking 'load more result' button - Fixed 'check_if_current_date_has_passed' function bug for Month End Scraper - Added more tests - Added logic to check in case the past year is entered for Thread Pool and Month End scraper - Added timezone parameter for Thread Pool and Month End scrapers so that they check the past date based on the entered timezone, mostly for fixing timezone problems when testing using GitHub Action - Added timezone parameter for 'check_if_current_date_has_passed' mostly for fixing timezone problems when testing using GitHub Action - Adjusted log message - Added ElementClickInterceptedException handler when clicking pop-up ad and the load more result button - Added NoSuchWindowException handler while scrolling down the browser window - Added finally block to ensure that the driver is closed - Handle case when HTML content is None.

--- japan_avg_hotel_price_finder/configure_logging.py
+++ japan_avg_hotel_price_finder/configure_logging.py
@@ -1,36 +1,5 @@
-import concurrent.futures
 import logging
-import queue
-import threading
 
-
-class AsyncFileHandler(logging.Handler):
-    def __init__(self, filename, mode='a'):
-        super().__init__()
-        self.filename = filename
-        self.mode = mode
-        self.log_queue = queue.Queue()
-        self._stop_event = threading.Event()
-        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-        self.executor.submit(self._process_log_queue)
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.log_queue.put(log_entry)
-
-    def _process_log_queue(self):
-        while not self._stop_event.is_set() or not self.log_queue.empty():
-            try:
-                log_entry = self.log_queue.get(timeout=1)
-                with open(self.filename, self.mode) as log_file:
-                    log_file.write(log_entry + '\n')
-            except queue.Empty:
-                continue
-
-    def close(self):
-        self._stop_event.set()
-        self.executor.shutdown(wait=True)
-        super().close()
 
 def configure_logging(logger_name='root') -> None | logging.Logger:
     """
@@ -86,8 +55,8 @@ def configure_logging_with_file(log_file, logger_name='root') -> None | logging.
     # Define a custom log format
     log_format = '%(asctime)s | %(filename)s | line:%(lineno)d | %(funcName)s | %(levelname)s | %(message)s'
 
-    # Create an AsyncFileHandler to write logs to the specified file
-    async_file_handler = AsyncFileHandler(log_file, mode='a')
+    # Create a FileHandler to write logs to the specified file in overwrite mode
+    file_handler = logging.FileHandler(log_file, mode='w')  # 'w' for write mode (overwrite)
 
     # Create a StreamHandler to output logs to the terminal
     stream_handler = logging.StreamHandler()
@@ -96,11 +65,11 @@ def configure_logging_with_file(log_file, logger_name='root') -> None | logging.
     formatter = logging.Formatter(log_format)
 
     # Set the Formatter for both the FileHandler and StreamHandler
-    async_file_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
     stream_handler.setFormatter(formatter)
 
     # Add both the FileHandler and StreamHandler to the root logger
-    logger.addHandler(async_file_handler)
+    logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
     return logger
