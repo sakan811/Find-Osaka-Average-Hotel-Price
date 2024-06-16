ae831d0b26912d8cc2974e1b28205b8e10fa296c
Sakan Nirattisaykul <94357278+sakan811@users.noreply.github.com>
2024-06-16 15:12:33 +0700

    Update migrate_to_sqlite.py

--- japan_avg_hotel_price_finder/migrate_to_sqlite.py
+++ japan_avg_hotel_price_finder/migrate_to_sqlite.py
@@ -35,6 +35,7 @@ def migrate_data_to_sqlite(df_filtered: pd.DataFrame, details: Details) -> None:
 
     db = details.sqlite_name
     with sqlite3.connect(db) as con:
+        con.execute("PRAGMA journal_mode=WAL")
         query = '''
         CREATE TABLE IF NOT EXISTS HotelPrice (
             ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -85,6 +86,7 @@ def create_average_room_price_by_date_view(db: str) -> None:
     :return: None
     """
     with sqlite3.connect(db) as con:
+        con.execute("PRAGMA journal_mode=WAL")
         query = '''
         CREATE VIEW IF NOT EXISTS AverageRoomPriceByDate AS 
         select Date, avg(Price) as AveragePrice, City
