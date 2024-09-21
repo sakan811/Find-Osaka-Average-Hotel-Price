ca5f048b59589cb7278fec9f121cdf2285b5063a
Sakan <sakanbeer@gmail.com>
2024-09-21 17:14:36 +0700

    refactor: Refactored codes

--- tests/test_file_processor/test_find_csv_files.py
+++ tests/test_file_processor/test_find_csv_files.py
@@ -2,13 +2,13 @@ from japan_avg_hotel_price_finder.file_processor.file_processor import find_csv_
 
 
 def test_find_csv_files():
-    directory = 'tests/test_utils/test_find_csv_files'
+    directory = 'tests/test_file_processor/test_find_csv_files'
     csv_files = find_csv_files(directory)
     print(csv_files)
     assert len(csv_files) > 0
 
 
 def test_find_csv_files_empty():
-    directory_2 = 'tests/test_utils/test_find_csv_files_2'
+    directory_2 = 'tests/test_file_processor/test_find_csv_files_2'
     csv_files = find_csv_files(directory_2)
     assert len(csv_files) == 0
